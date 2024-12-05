fe9ba2e109a37446be6b450620c1add6a4a89299
docaothangg <94737261+docaothangg@users.noreply.github.com>
2024-12-05 14:18:03 +0700

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB=async()=> {
-    await mongoose.connect('mongodb+srv://vantiz:khongbiet123@cluster0.fdcom.mongodb.net/food-del').then(()=>console.log("DB connected"));
-}
